081ad0a5e1e7c2f83f029a8c528e03207816dc06
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2022-11-20 19:46:44 -0300

    Ajustados os contrutores e definido apenas o método desenhar um dígito como público

--- app/src/main/java/engtelecom/poo/Display7Segmentos.java
+++ app/src/main/java/engtelecom/poo/Display7Segmentos.java
@@ -19,17 +19,17 @@ public class Display7Segmentos {
     /**
      * Valor de escala padrão
      */
-    private static final int ESCALA_PADRAO = 200;
+    private final int ESCALA_PADRAO = 100;
 
     /**
      * Valor de cor padrão para segmentos acesos
      */
-    private static final Color COR_ACESO_PADRAO = Color.RED;
+    private final Color COR_ACESO_PADRAO = Color.RED;
 
     /**
      * Valor de cor padrão para segmentos apagados
      */
-    private static final Color COR_APAGADO_PADRAO = Color.LIGHT_GRAY;
+    private final Color COR_APAGADO_PADRAO = Color.LIGHT_GRAY;
 
     /**
      * Arranjo dos sete segmentos que compões um dígito
@@ -49,7 +49,7 @@ public class Display7Segmentos {
     /**
      * Cor de cada segmento
      */
-    private Color[] corSegmento = new Color[7];
+    private Color[] corSegmento;
 
     /**
      * Cor dos segmentos apagados
@@ -64,7 +64,7 @@ public class Display7Segmentos {
     /**
      * Tamanho proporcional do dígito
      */
-    private int escala;
+    private double escala;
 
 
     /**
@@ -75,12 +75,14 @@ public class Display7Segmentos {
      * @param corApagado cor representando os segmentos apagados
      * @param escala tamanho do display na tela como um todo
      */
-    public Display7Segmentos(Draw tela, double[] coordenadas, Color corAceso, Color corApagado, int escala) {
+    public Display7Segmentos(Draw tela, double[] coordenadas, Color corAceso, Color corApagado, double escala) {
         this.tela = tela;
         this.coordenadas = coordenadas;
         this.corApagado = corApagado;
         this.corAceso = corAceso;
         this.escala = escala;
+        this.corSegmento = new Color[7];
+
     }
 
     /**
@@ -91,7 +93,12 @@ public class Display7Segmentos {
      * @param corApagado cor representando os segmentos apagados
      */
     public Display7Segmentos(Draw tela, double[] coordenadas, Color corAceso, Color corApagado) {
-        this(tela, coordenadas, corAceso, corApagado, ESCALA_PADRAO);
+        this.tela = tela;
+        this.coordenadas = coordenadas;
+        this.corAceso = corAceso;
+        this.corApagado = corApagado;
+        this.escala = ESCALA_PADRAO;
+        this.corSegmento = new Color[7];
     }
 
     /**
@@ -101,7 +108,12 @@ public class Display7Segmentos {
      * @param escala tamanho do display na tela como um todo
      */
     public Display7Segmentos(Draw tela, double[] coordenadas, int escala) {
-        this(tela, coordenadas, COR_ACESO_PADRAO, COR_APAGADO_PADRAO, escala);
+        this.tela = tela;
+        this.coordenadas = coordenadas;
+        this.corAceso = COR_ACESO_PADRAO;
+        this.corApagado = COR_APAGADO_PADRAO;
+        this.escala = escala;
+        this.corSegmento = new Color[7];
     }
 
     /**
@@ -110,7 +122,12 @@ public class Display7Segmentos {
      * @param coordenadas posição central onde o display será desenhado
      */
     public Display7Segmentos(Draw tela, double[] coordenadas) {
-        this(tela, coordenadas, COR_ACESO_PADRAO, COR_APAGADO_PADRAO);
+        this.tela = tela;
+        this.coordenadas = coordenadas;
+        this.corAceso = COR_ACESO_PADRAO;
+        this.corApagado = COR_APAGADO_PADRAO;
+        this.escala = ESCALA_PADRAO;
+        this.corSegmento = new Color[7];
     }
 
     /**
@@ -118,7 +135,7 @@ public class Display7Segmentos {
      * com o dígito informado
      * @param digito valor inteiro de 0 a 9. Caso maior que 9, a letra "E" é desenhada
      */
-    public void desenhar(int digito){
+    public boolean desenhar(int digito){
         // Definindo as cores iniciais de cada segmento
         for (int i = 0; i < corSegmento.length; i++) this.corSegmento[i] = corAceso;
 
@@ -160,72 +177,77 @@ public class Display7Segmentos {
             // Caso o dígito seja maior que 9, exibe "E"
             // no display, indicando erro
             for (int i = 1; i < 3; i++) this.corSegmento[i] = corApagado;
+            return ! desenhar(this.corSegmento);
         }
 
-        desenhar(this.corSegmento);
+        return desenhar(this.corSegmento);
     }
 
     /**
      * Desenha um display com 7 segmentos
      * @param corSegmento define a cor de cada segmento
      */
-    public void desenhar(Color[] corSegmento){
-        // Coordenadas iniciais
-        double xInicial = coordenadas[0] - (0.5*escala);
-        double yInicial = coordenadas[1] - (1.05*escala);
+    private boolean desenhar(Color[] corSegmento){
+        if (this.tela == null | escala <= 0) return false;
+        else {
+            // Coordenadas iniciais
+            double xInicial = coordenadas[0] - (0.5*escala);
+            double yInicial = coordenadas[1] - (1.05*escala);
 
-        // -------------------------------------- //
-        double xHorizontal = xInicial;
-        double yHorizontal = yInicial;
+            // Segmentos horizontais ---------------- //
+            double xHorizontal = xInicial;
+            double yHorizontal = yInicial;
 
-        // Desenhando o segmento horizontal D
-        this.segmento[3] = new Segmento(tela, xHorizontal, yHorizontal, escala, corSegmento[3]);
-        // -------------------------------------- //
+            // Desenhando o segmento horizontal D
+            this.segmento[3] = new Segmento(tela, xHorizontal, yHorizontal, escala, corSegmento[3]);
+            // -------------------------------------- //
 
-        // -------------------------------------- //
-        // Desenhando o segmento horizontal G
-        yHorizontal += MULTIPLICADOR_Y*escala;
-        this.segmento[6] = new Segmento(tela, xHorizontal, yHorizontal, escala, corSegmento[6]);
-        // -------------------------------------- //
+            // -------------------------------------- //
+            // Desenhando o segmento horizontal G
+            yHorizontal += MULTIPLICADOR_Y*escala;
+            this.segmento[6] = new Segmento(tela, xHorizontal, yHorizontal, escala, corSegmento[6]);
+            // -------------------------------------- //
 
-        // -------------------------------------- //
-        // Desenhando o segmento horizontal A
-        yHorizontal += MULTIPLICADOR_Y*escala;
-        this.segmento[0] = new Segmento(tela, xHorizontal, yHorizontal, escala, corSegmento[0]);
-        // -------------------------------------- //
-        
-        // -------------------------------------- //
-        double xVertical = xInicial;
-        double yVertical = yInicial;
+            // -------------------------------------- //
+            // Desenhando o segmento horizontal A
+            yHorizontal += MULTIPLICADOR_Y*escala;
+            this.segmento[0] = new Segmento(tela, xHorizontal, yHorizontal, escala, corSegmento[0]);
+            // -------------------------------------- //
+            
+            // Segmentos verticais ------------------ //
+            double xVertical = xInicial;
+            double yVertical = yInicial;
 
-        // Motando os pontos para desenhar o segmento vertical E
-        // Multiplicador padrão para alinhar à grade: 0.1
-        yVertical += MULTIPLICADOR_Y_INICIAL*escala;
-        this.segmento[4] = new Segmento(tela, xVertical, yVertical, escala, corSegmento[4], true);
-        // -------------------------------------- //
+            // Motando os pontos para desenhar o segmento vertical E
+            // Multiplicador padrão para alinhar à grade: 0.1
+            yVertical += MULTIPLICADOR_Y_INICIAL*escala;
+            this.segmento[4] = new Segmento(tela, xVertical, yVertical, escala, corSegmento[4], true);
+            // -------------------------------------- //
 
-        // -------------------------------------- //
-        // Desenhando o segmento vertical C
-        xVertical += 0.8*escala;
-        this.segmento[2] = new Segmento(tela, xVertical, yVertical, escala, corSegmento[2], true);
-        // -------------------------------------- //
+            // -------------------------------------- //
+            // Desenhando o segmento vertical C
+            xVertical += 0.8*escala;
+            this.segmento[2] = new Segmento(tela, xVertical, yVertical, escala, corSegmento[2], true);
+            // -------------------------------------- //
 
-        // -------------------------------------- //
-        // Desenhando o segmento vertical F
-        xVertical -= 0.8*escala;
-        yVertical += MULTIPLICADOR_Y*escala;
-        this.segmento[5] = new Segmento(tela, xVertical, yVertical, escala, corSegmento[5],true);
-        // -------------------------------------- //
+            // -------------------------------------- //
+            // Desenhando o segmento vertical F
+            xVertical -= 0.8*escala;
+            yVertical += MULTIPLICADOR_Y*escala;
+            this.segmento[5] = new Segmento(tela, xVertical, yVertical, escala, corSegmento[5],true);
+            // -------------------------------------- //
 
-        // -------------------------------------- //
-        // Desenhando o segmento vertical B
-        xVertical += 0.8*escala;
-        this.segmento[1] = new Segmento(tela, xVertical, yVertical, escala, corSegmento[1],true);
-        // -------------------------------------- //
+            // -------------------------------------- //
+            // Desenhando o segmento vertical B
+            xVertical += 0.8*escala;
+            this.segmento[1] = new Segmento(tela, xVertical, yVertical, escala, corSegmento[1],true);
+            // -------------------------------------- //
 
-        for (int i = 0; i < segmento.length; i++) {
-            this.segmento[i].desenhar();
-        }
+            for (int i = 0; i < segmento.length; i++) {
+                this.segmento[i].desenhar();
+            }
+            return true;
+        }   
     }
 
     /**
@@ -236,5 +258,59 @@ public class Display7Segmentos {
         this.coordenadas = coordenadas;
     }
 
-    
+    /**
+     * Define onde o display de 7 segmentos será desenhado
+     * @param tela objeto do tipo Draw
+     */
+    public void setTela(Draw tela) {
+        this.tela = tela;
+    }
+
+    /**
+     * Define a cor dos segmentos apagados
+     * @param corApagado
+     */
+    public void setCorApagado(Color corApagado) {
+        this.corApagado = corApagado;
+    }
+
+    /**
+     * Define a cor dos segmentos acesos
+     * @param corAceso
+     */
+    public void setCorAceso(Color corAceso) {
+        this.corAceso = corAceso;
+    }
+
+    /**
+     * Define o tamanho relativo do display
+     * @param escala valor com o novo tamanho
+     */
+    public void setEscala(double escala) {
+        this.escala = escala;
+    }
+
+    /**
+     * Obtém a cor atual do segmento apagado
+     * @return objeto do tipo Color representando a cor do segmento apagado.
+     */
+    public Color getCorApagado() {
+        return corApagado;
+    }
+
+    /**
+     * Obtém a cor atual do segmento aceso
+     * @return objeto do tipo Color representando a cor do segmento aceso.
+     */
+    public Color getCorAceso() {
+        return corAceso;
+    }
+
+    /**
+     * Obtém a escala atual
+     * @return double contendo o valor atual de escala.
+     */
+    public double getEscala() {
+        return escala;
+    }
 }
