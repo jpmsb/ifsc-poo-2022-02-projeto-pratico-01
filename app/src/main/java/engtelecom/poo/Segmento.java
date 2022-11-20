1154602ea0f3b8b0648d49866b56107c98f52125
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2022-11-20 17:55:03 -0300

    Ajustados os contrutores e removidos métodos redundantes

--- app/src/main/java/engtelecom/poo/Segmento.java
+++ app/src/main/java/engtelecom/poo/Segmento.java
@@ -7,17 +7,17 @@ public class Segmento {
     /**
      * Constante que define o tamanho padrão do desenho do segmento
      */
-    private static final int ESCALA_PADRAO = 200;
+    private final double ESCALA_PADRAO = 100;
 
     /**
      * Constante que define a cor padrão para vermelho
      */
-    private static final Color COR_PADRAO = Color.RED;
+    private final Color COR_PADRAO = Color.RED;
 
     /**
      * Constante que define as coordenadas padrões
      */
-    private static final double[] COORDENADAS_PADRAO = {0,0};
+    private final double[] COORDENADAS_PADRAO = {0,0};
 
     /**
      * Objeto onde será desenhado o segmento
@@ -32,7 +32,7 @@ public class Segmento {
     /**
      * Tamanho do segmento
      */
-    private int escala;
+    private double escala;
 
     /**
      * Coordenada inferior esquerda X do segmento
@@ -53,7 +53,8 @@ public class Segmento {
     /**
      * Construtor da classe
      * 
-     * Cria um segmento definindo todos os seus atributos
+     * Cria um segmento definindo todos os seus atributos. Todos os atributos passados
+     * devem ser válidos.
      * 
      * @param tela objeto onde o segmento será desenhado
      * @param coordenadaX coordenada inferior esquerda X de onde o segmento será desenhado
@@ -62,7 +63,7 @@ public class Segmento {
      * @param cor cor do segmento
      * @param tipoVertical tipo de orientação do segmento. Verdadeiro para ser vertical e falso para horizontal
      */
-    public Segmento(Draw tela, double coordenadaX, double coordenadaY, int escala, Color cor, boolean tipoVertical) {
+    public Segmento(Draw tela, double coordenadaX, double coordenadaY, double escala, Color cor, boolean tipoVertical) {
         this.tela = tela;
         this.escala = escala;
         this.coordenadaX = coordenadaX;
@@ -74,7 +75,7 @@ public class Segmento {
     /**
      * Construtor da classe
      * 
-     * Cria um segmento definindo o tipo como horizontal
+     * Cria um segmento horizontal
      * 
      * @param tela objeto onde o segmento será desenhado
      * @param coordenadaX coordenada inferior esquerda X de onde o segmento será desenhado
@@ -82,7 +83,7 @@ public class Segmento {
      * @param escala tamanho proporcional do segmento
      * @param cor cor do segmento
      */
-    public Segmento(Draw tela, double coordenadaX, double coordenadaY, int escala, Color cor){
+    public Segmento(Draw tela, double coordenadaX, double coordenadaY, double escala, Color cor){
         this(tela, coordenadaX, coordenadaY, escala, cor, false);
     }
 
@@ -96,8 +97,12 @@ public class Segmento {
      * @param coordenadaY coordenada inferior esquerda Y de onde o segmento será desenhado
      * @param escala tamanho proporcional do segmento
      */
-    public Segmento(Draw tela, double coordenadaX, double coordenadaY, int escala) {
-        this(tela, coordenadaX, coordenadaY, escala, COR_PADRAO);
+    public Segmento(Draw tela, double coordenadaX, double coordenadaY, double escala) {
+        this.tela = tela;
+        this.coordenadaX = coordenadaX;
+        this.coordenadaY = coordenadaY;
+        this.escala = escala;
+        this.cor = COR_PADRAO;
     }
 
     /**
@@ -110,7 +115,12 @@ public class Segmento {
      * @param coordenadaY coordenada inferior esquerda Y de onde o segmento será desenhado
      */
     public Segmento(Draw tela, double coordenadaX, double coordenadaY) {
-        this(tela, coordenadaX, coordenadaY, ESCALA_PADRAO);
+        this.tela = tela;
+        this.coordenadaX = coordenadaX;
+        this.coordenadaY = coordenadaY;
+        this.escala = ESCALA_PADRAO;
+        this.cor = COR_PADRAO;
+
     }
 
     /**
@@ -121,8 +131,12 @@ public class Segmento {
      * @param tela objeto onde o segmento será desenhado
      * @param escala tamanho proporcional do segmento
      */
-    public Segmento(Draw tela, int escala) {
-        this(tela, COORDENADAS_PADRAO[0], COORDENADAS_PADRAO[1], escala);
+    public Segmento(Draw tela, double escala) {
+        this.tela = tela;
+        this.escala = escala;
+        this.coordenadaX = COORDENADAS_PADRAO[0];
+        this.coordenadaY = COORDENADAS_PADRAO[1];
+        this.cor = COR_PADRAO;
     }
 
     /**
@@ -133,92 +147,153 @@ public class Segmento {
      * @param tela objeto onde o segmento será desenhado
      */
     public Segmento(Draw tela){
-        this(tela, COORDENADAS_PADRAO[0], COORDENADAS_PADRAO[1]);
+        this.tela = tela;
+        this.escala = ESCALA_PADRAO;
+        this.coordenadaX = COORDENADAS_PADRAO[0];
+        this.coordenadaY = COORDENADAS_PADRAO[1];
+        this.cor = COR_PADRAO;
     }
 
     /**
      * Cria e desenha um segmento horizontal com cor e coordenadas
      * definas no construtor
+     * @return verdadeiro caso seja possível desenhar e falso, caso contrário.
      */
-    public void horizontal(){
-        horizontal(this.cor, this.coordenadaX, this.coordenadaY);
-    }
+    public boolean horizontal(){
+        if (this.tela == null | escala <= 0) return false;
+        else {
+            double[] xSegmento = {0.1*escala+coordenadaX, 0.2*escala+coordenadaX, 0.8*escala+coordenadaX, 0.9*escala+coordenadaX, 0.8*escala+coordenadaX, 0.2*escala+coordenadaX};
+            double[] ySegmento = {0.2*escala+coordenadaY, 0.3*escala+coordenadaY, 0.3*escala+coordenadaY, 0.2*escala+coordenadaY, 0.1*escala+coordenadaY, 0.1*escala+coordenadaY};
 
-    /**
-     * Cria e desenha um segmento horizontal especificando uma cor
-     * @param cor cor do segmento
-     */
-    public void horizontal(Color cor){
-        horizontal(cor, this.coordenadaX, this.coordenadaY);
-    }
-
-    /**
-     * Cria e desenha um segmento horizontal especificando as coordenadas
-     * @param coordenadaX coordenada inferior esquerda X
-     * @param coordenadaY coordenada inferior esquerda Y
-     */
-    public void horizontal(double coordenadaX, double coordenadaY){
-        horizontal(this.cor, coordenadaX, coordenadaY);
-    }
-
-    /**
-     * Cria e desenha um segmento horizontal especificando cor e coordenadas
-     * @param cor cor do segmento
-     * @param coordenadaX coordenada inferior esquerda X
-     * @param coordenadaY coordenada inferior esquerda Y
-     */
-    public void horizontal(Color cor, double coordenadaX, double coordenadaY){
-        double[] xSegmento = {0.1*this.escala+coordenadaX, 0.2*this.escala+coordenadaX, 0.8*this.escala+coordenadaX, 0.9*this.escala+coordenadaX, 0.8*this.escala+coordenadaX, 0.2*this.escala+coordenadaX};
-        double[] ySegmento = {0.2*this.escala+coordenadaY, 0.3*this.escala+coordenadaY, 0.3*this.escala+coordenadaY, 0.2*this.escala+coordenadaY, 0.1*this.escala+coordenadaY, 0.1*this.escala+coordenadaY};
-
-        this.tela.setPenColor(cor);
-        this.tela.filledPolygon(xSegmento, ySegmento);    
+            this.tela.setPenColor(cor);
+            this.tela.filledPolygon(xSegmento, ySegmento);
+            return true;
+        }
     }
 
     /**
      * Cria e desenha um segmento vertical com cor e coordenadas
      * definas no construtor
+     * @return verdadeiro caso seja possível desenhar e falso, caso contrário.
      */
-    public void vertical(){
-        vertical(this.cor, this.coordenadaX, this.coordenadaY);
-    }
+    public boolean vertical(){
+        if (this.tela == null | escala <= 0) return false;
+        else {
+            double[] xSegmento = {0.1*escala+coordenadaX, 0.2*escala+coordenadaX, 0.2*escala+coordenadaX, 0.1*escala+coordenadaX, 0*escala+coordenadaX, 0*escala+coordenadaX};
+            double[] ySegmento = {0.2*escala+coordenadaY, 0.3*escala+coordenadaY, 0.9*escala+coordenadaY, 1.0*escala+coordenadaY, 0.9*escala+coordenadaY, 0.3*escala+coordenadaY};
 
-    /**
-     * Cria e desenha um segmento vertical especificando uma cor
-     * @param cor cor do segmento
-     */
-    public void vertical(Color cor){
-        horizontal(cor, this.coordenadaX, this.coordenadaY);
-    }
-
-    /**
-     * Cria e desenha um segmento vertical especificando as coordenadas
-     * @param coordenadaX coordenada inferior esquerda X
-     * @param coordenadaY coordenada inferior esquerda Y
-     */
-    public void vertical(double coordenadaX, double coordenadaY){
-        vertical(this.cor, coordenadaX, coordenadaY);
-    }
-
-    /**
-     * Cria e desenha um segmento vertical especificando cor e coordenadas
-     * @param cor cor do segmento
-     * @param coordenadaX coordenada inferior esquerda X
-     * @param coordenadaY coordenada inferior esquerda Y
-     */
-    public void vertical(Color cor, double coordenadaX, double coordenadaY){
-        double[] xSegmento = {0.1*escala+coordenadaX, 0.2*escala+coordenadaX, 0.2*escala+coordenadaX, 0.1*escala+coordenadaX, 0*escala+coordenadaX, 0*escala+coordenadaX};
-        double[] ySegmento = {0.2*escala+coordenadaY, 0.3*escala+coordenadaY, 0.9*escala+coordenadaY, 1.0*escala+coordenadaY, 0.9*escala+coordenadaY, 0.3*escala+coordenadaY};
-
-        this.tela.setPenColor(cor);
-        this.tela.filledPolygon(xSegmento, ySegmento);
+            this.tela.setPenColor(cor);
+            this.tela.filledPolygon(xSegmento, ySegmento);
+            return true;
+        }
     }
 
     /**
      * Desenha o segmento de acordo com o tipo definido no construtor
+     * @return string contendo a orientação atual
      */
-    public void desenhar(){
-        if (tipoVertical) vertical();
-        else horizontal();
+    public String desenhar(){
+        if (tipoVertical && vertical()) {
+            return "Vertical";
+        } else if ((! tipoVertical) && horizontal()) {
+            return "Horizontal";
+        } else return "Inválido";
+    }
+
+    /**
+     * Define o objeto onde o segmento será desenhado
+     * @param tela objeto onde o segmento será desenhado
+     */    
+    public void setTela(Draw tela) {
+        this.tela = tela;
+    }
+
+    /**
+     * Define a cor do segmento
+     * @param cor cor do segmento
+     */
+    public void setCor(Color cor) {
+        this.cor = cor;
+    }
+
+    /**
+     * Define o tamanho proporcional do segmento
+     * @param escala tamanho do segmento
+     */
+    public void setEscala(double escala) {
+        this.escala = escala;
+    }
+
+    /**
+     * Define a coordenada X
+     * @param coordenadaX valor representando a posição horizontal
+     */
+    public void setCoordenadaX(double coordenadaX) {
+        this.coordenadaX = coordenadaX;
+    }
+
+    /**
+     * Define a coordenada Y
+     * @param coordenadaY valor representando a posição vertical
+     */
+    public void setCoordenadaY(double coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+
+    /**
+     * Define se o segmento é horizontal ou vertical.
+     * Se for vertical, deve ser definido "true".
+     * @param tipoVertical verdadeiro ou falso para ser vertical
+     */
+    public void setTipoVertical(boolean tipoVertical) {
+        this.tipoVertical = tipoVertical;
+    }
+
+    /**
+     * Obtém o objeto onde o segmento será desenhado
+     * @return objeto do tipo Draw
+     */
+    public Draw getTela() {
+        return tela;
+    }
+
+    /**
+     * Obtém a cor atual do segmento
+     * @return cor atual
+     */
+    public Color getCor() {
+        return cor;
+    }
+
+    /**
+     * Obtém a escala atual do segmento
+     * @return escala 
+     */
+    public double getEscala() {
+        return escala;
+    }
+
+    /**
+     * Obtém a coordenada X do segmento
+     * @return double com o valor da coordenada X
+     */
+    public double getCoordenadaX() {
+        return coordenadaX;
+    }
+
+    /**
+     * Obtém a coordenada Y do segmento
+     * @return double com o valor da coordenada Y
+     */
+    public double getCoordenadaY() {
+        return coordenadaY;
+    }
+
+    /**
+     * Obtém o tipo da orientação do segmento.
+     * @return verdadeiro caso a orientação seja vertical e falso, caso contrário.
+     */
+    public boolean isTipoVertical() {
+        return tipoVertical;
     }
 }
