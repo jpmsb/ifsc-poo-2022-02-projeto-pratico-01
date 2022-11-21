bb68af04a4b7e0926e3950d58d47ba073b0c0899
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2022-11-20 22:40:51 -0300

    Criado um programa que testa o cronômetro e o contador regressivo

--- app/src/main/java/engtelecom/poo/Principal.java
+++ app/src/main/java/engtelecom/poo/Principal.java
@@ -1,7 +1,7 @@
 package engtelecom.poo;
 
-import java.awt.Color;
 import java.time.LocalTime;
+import java.awt.Color;
 
 import edu.princeton.cs.algs4.Draw;
 
@@ -13,32 +13,20 @@ public class Principal {
         d.setYscale(0, 800);
         d.enableDoubleBuffering();
 
-        d.clear(d.BLACK);
+        d.clear(Color.BLACK);
 
         int hora = LocalTime.now().getHour();
         int minuto = LocalTime.now().getMinute();
         int segundo = LocalTime.now().getSecond();
-        Cronometro cro = new Cronometro(d, new double[]{400,200}, 100, d.PINK, hora, minuto, segundo);
+        Cronometro cro = new Cronometro(d, new double[]{400,200}, 100, Color.GREEN, hora, minuto, segundo);
 
-        // while (true){
-        //     d.clear(d.BLACK);
-        //     cro.contar();
-        //     d.show();
-        //     try {
-        //         Thread.sleep(1000);
-        //     } catch (InterruptedException e) {
-        //         // TODO Auto-generated catch block
-        //         e.printStackTrace();
-        //     }
-        // }
-
-        ContadorRegressivo cr = new ContadorRegressivo(d, new double[]{400,600}, 100, d.PINK, 99,59,59);
+        ContadorRegressivo cr = new ContadorRegressivo(d, new double[]{400,600}, 100, d.PINK, 1,30,00);
 
         while (true){
-            d.clear(d.BLACK);
-            cro.contar();
-            cr.contar();
+            cro.conta();
+            cr.conta();
             d.show();
+            d.clear(Color.BLACK);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -46,39 +34,5 @@ public class Principal {
                 e.printStackTrace();
             }
         }
-
-        // Display7Segmentos s = new Display7Segmentos(d, new double[]{400,400}, Color.RED, Color.BLACK,100);
-        // s.desenhar(8);
-
-
-        // Animação para testar o posicionamento do display
-        // int i = 1;
-        // while(true) {
-        //     d.clear();
-        //     s.desenhar(8);
-        //     s.setCoordenadas(new int[]{(i*10)%800,(i*10)%800});
-        //     try {
-        //         Thread.sleep(20);
-        //     } catch (InterruptedException e) {
-        //         // TODO Auto-generated catch block
-        //         e.printStackTrace();
-        //     }
-        //     i++;
-        // }
-
-
-        // int i = 0;
-        // while (true) {
-        //     d.clear(Color.LIGHT_GRAY);
-        //     // s.desenhar(i%11);
-        //     cr.mostrar(i%11);
-                // d.show();
-        //     try {
-        //         Thread.sleep(1000);
-        //     } catch (InterruptedException e) {
-        //         e.printStackTrace();
-        //     }
-        //     i++;
-        // }
     }
 }
